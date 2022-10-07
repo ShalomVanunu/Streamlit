b067e81c66b8dd4d9587f8492bc311b04a8f1653
shalomvanunu <shalomvanunu@gmail.com>
2022-10-07 20:40:20 +0300

    Updated 7/10/22

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+
+st.title("write title : st.title() :tada:") #https://www.webfx.com/tools/emoji-cheat-sheet/
+st.write(" icons from https://www.webfx.com/tools/emoji-cheat-sheet/")
+
+st.header("write header : st.header() ")
+st.subheader("write subheader : st.subheader() ")
+st.write("write text or variable : st.write() ")
