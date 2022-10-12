e8dd65ec4efda34dcc50c72b561e9a3d87a6111a
shalomvanunu <shalomvanunu@gmail.com>
2022-10-12 19:33:35 +0300

    Updated 12/10/22

--- pages/05_files.py
+++ pages/05_files.py
@@ -1,4 +1,17 @@
 #Youtube
 #https://www.youtube.com/watch?v=vIQQR_yq-8I
+import pandas as pd
 import streamlit as st
+
+
+file_data = st.file_uploader("st.file_uploader()  ---> ", type=["csv"])
+
+if file_data is not None:
+    df = pd.read_csv(file_data)
+    st.dataframe(df)
+    st.download_button(
+        label="st.download_button()  ---> Download data as CSV",
+        data=file_data,
+        file_name='large_df.csv',
+        mime='text/csv', )
 
