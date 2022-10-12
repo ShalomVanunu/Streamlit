e6df8b56fc253bf66f3430bc04d47c2c094db1f7
shalomvanunu <shalomvanunu@gmail.com>
2022-10-12 17:42:23 +0300

    Updated 12/10/22

--- app.py
+++ app.py
@@ -1,32 +1,19 @@
 import streamlit as st
 
-def load_file(name):
-    with open(name,'rb') as f:
-        data = f.read()
-        return data
-
-
-col1, col2= st.columns(2)
-
-with col1:
-    st.write(" add column : st.columns(1)")
-    data_file = load_file("streamlit.png")
-    st.image(data_file, width=100)
-with col2:
-    st.write(" add column : st.columns(2)")
-    st.write("Referance https://docs.streamlit.io/library/api-reference ")
-
-
-
-st.title("import streamlit as st")
+st.title("Welcome :tada:") #https://www.webfx.com/tools/emoji-cheat-sheet/
 st.write("------")
-st.title("write title : st.title() :tada:") #https://www.webfx.com/tools/emoji-cheat-sheet/
-st.write(" icons from https://www.webfx.com/tools/emoji-cheat-sheet/")
 
-st.header("write header : st.header() ")
-st.subheader("write subheader : st.subheader() ")
-st.write("write text or variable : st.write() ")
-st.caption("write caption : st.caption() ")
-st.code("write code : st.code() ")
+st.code(""" ## create python file . ex: app.py
+import streamlit as st
 
+st.write(" text ")
+
+## to Run the code
+## in Terminal CLI : > streamlit run app.py
+
+
+""")
+
+st.write("------")
+st.subheader(" Created by :copyright:Shalom Vanunu :computer:")
 
